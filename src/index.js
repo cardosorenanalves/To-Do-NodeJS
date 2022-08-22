70b7cd7e082659df6739cd6e6853fb5dc207cf17
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-22 11:50:21 -0300

    Feat: middleware checkExistUserAccount create

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
+  const {username} = request.headers;
 
+  const user = users.find(customer => customer.username === username);
+
+  if(!user){
+      return response.status(400).json({error: "User not found!"})
+  }
+
+  request.user = user;
+  
+  return next()
 }
 
 app.post('/users', (request, response) => {
