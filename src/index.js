f0c7fd399abb73ac25d2615ec46600c3ca639a9d
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-22 11:46:15 -0300

    feat: Route Post /users create

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const usernameExists = users.some((user) => 
+  user.username === username
+  )
+
+  if(usernameExists){
+    return response.status(400).json({error: 'Username already exists!'})
+  }
+
+ users.push({ 
+	id: uuidv4(),
+	name, 
+	username, 
+	todos: []
+}) 
+
+return response.status(201).send()
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
